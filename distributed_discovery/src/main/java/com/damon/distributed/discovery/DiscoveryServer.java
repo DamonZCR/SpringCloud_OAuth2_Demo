e3ee876582ce6e3577b718ea7af088f337534046
damonzlw <137593938@qq.com>
2022-07-13 08:49:12 +0800

    4-构建

---
+++ distributed_discovery/src/main/java/com/damon/distributed/discovery/DiscoveryServer.java
@@ -0,0 +1,17 @@
+package com.damon.distributed.discovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author Administrator
+ * @version 1.0
+ **/
+@SpringBootApplication
+@EnableEurekaServer//表明自己是eureka启动类，开始启动
+public class DiscoveryServer {
+    public static void main(String[] args) {
+        SpringApplication.run(DiscoveryServer.class,args);
+    }
+}
